a82bfee243d801c6c75bf985629e87c6908b48ea
shay876 <111309646+shay876@users.noreply.github.com>
2024-02-16 11:50:26 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BranchingAsst2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("What is the package weight?");
+
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("What is the package width?");
+
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("What is the package height?");
+
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("What is the package length?");
+
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            if ((width + height + length) > 50)
+            { Console.WriteLine("Package too big to be shipped via Package Express"); }
+
+            else
+            {
+                Console.WriteLine("Your estimated total for shipping is $" + ((height * width * length) * weight) / 100 + ". Thank you!");
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
+
